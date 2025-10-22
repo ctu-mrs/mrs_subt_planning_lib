8b1888c4b813a08efb82fa88eced16d17e1f5456
Valentin Sebille <sebillev22@gmail.com>
2025-10-22 11:34:28 +0200

    refactor pcl_map.cpp

--- src/pcl_map.cpp
+++ src/pcl_map.cpp
@@ -1,4 +1,4 @@
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <mrs_subt_planning_lib/pcl_map.h>
 
 using namespace mrs_subt_planning;
@@ -14,27 +14,27 @@ PCLMap::~PCLMap(void) {
 
 float PCLMap::radiusSearch(const double x, const double y, const double z, const double search_radius) {
   const pcl::PointXYZ point(x, y, z);
-  /* ROS_WARN("[%s]: Calling radius search, point [%.2f, %.2f, %.2f], search_radius = %.2f", ros::this_node::getName().c_str(), x, y, z, search_radius); */
+  /* RCLCPP_WARN(this->get_logger()," Calling radius search, point [%.2f, %.2f, %.2f], search_radius = %.2f", x, y, z, search_radius); */
   std::vector<int>   k_indices;
   std::vector<float> k_sqr_distances;
   /* float result; */
   octree->radiusSearch(point, search_radius, k_indices, k_sqr_distances, 25);
   if (k_sqr_distances.size() > 0) {
-    /* ROS_INFO("[%s]: Point found, size = %lu", ros::this_node::getName().c_str(), k_sqr_distances.size()); */
+    /* RCLCPP_INFO(this->get_logger(),"Point found, size = %lu", k_sqr_distances.size()); */
     /* result = *std::min_element(k_sqr_distances.begin(), k_sqr_distances.end()); */
-    /* ROS_INFO("[%s]: Smallest dist = %.2f", ros::this_node::getName().c_str(), result); */
+    /* RCLCPP_INFO(this->get_logger(),"Smallest dist = %.2f", result); */
     return sqrt(*std::min_element(k_sqr_distances.begin(), k_sqr_distances.end()));
   }
   /* k_indices.resize(1); */
   /* k_sqr_distances.resize(1); */
   /* if (octree->nearestKSearch(point, 1, k_indices, k_sqr_distances) > -1) { */
-  /*   /1* ROS_WARN("[%s]: result = %.2f", ros::this_node::getName().c_str(), sqrt(k_sqr_distances[0])); *1/ */
+  /*   /1* RCLCPP_WARN(this->get_logger(),"result = %.2f", sqrt(k_sqr_distances[0])); *1/ */
   /*   if (sqrt(k_sqr_distances[0]) < search_radius) { */
-  /*     /1* ROS_WARN("[%s]: returning result = %.2f", ros::this_node::getName().c_str(), sqrt(k_sqr_distances[0])); *1/ */
+  /*     /1* RCLCPP_WARN(this->get_logger(),"returning result = %.2f", sqrt(k_sqr_distances[0])); *1/ */
   /*     return sqrt(k_sqr_distances[0]); */
   /*   } */
   /* } */
-  /* ROS_WARN("[%s]: Returning -1", ros::this_node::getName().c_str()); */
+  /* RCLCPP_WARN(this->get_logger(),"Returning -1"); */
   return -1;
 }
 
@@ -73,20 +73,20 @@ void PCLMap::loadMap(const std::string &filepath, double resolution) {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   if (pcl::io::loadPCDFile<pcl::PointXYZ>(filepath.c_str(), *cloud) == -1)  // load the file
   {
-    ROS_ERROR("Couldn't read file %s\n", filepath.c_str());
+    RCLCPP(this->get_logger(),"Couldn't read file %s\n", filepath.c_str());
     return;
   }
 
-  ROS_INFO_STREAM("Loaded " << cloud->width * cloud->height << " data points from " << filepath);
+  RCLCPP_INFO_STREAM(this->get_logger(),"Loaded " << cloud->width * cloud->height << " data points from " << filepath);
   octree->deleteTree();
   // Initialize octree-based point cloud change detection class
   octree->setInputCloud(cloud);
   // Add points from cloud to octree
   octree->addPointsFromInputCloud();
   /* octree->setResolution(0.1); */
-  ROS_INFO("[%s]: Tree depth = %d for resolution %.2f", ros::this_node::getName().c_str(), octree->getTreeDepth(), resolution);
+  RCLCPP_INFO(this->get_logger(),"Tree depth = %d for resolution %.2f", octree->getTreeDepth(), resolution);
   pcl_cloud = cloud;
-  ROS_INFO("[%s]: Testing distance from [3.9, 20.6, 8.70] = %.2f", ros::this_node::getName().c_str(), radiusSearch(4.8, 17.9, 8.7, 85.0));
+  RCLCPP_INFO(this->get_logger(),"Testing distance from [3.9, 20.6, 8.70] = %.2f", radiusSearch(4.8, 17.9, 8.7, 85.0));
 }
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr PCLMap::getPCLCloud() {
@@ -97,7 +97,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr PCLMap::pclVectorToPointcloud(const std::vec
   pcl::PointCloud<pcl::PointXYZ>::Ptr data(new pcl::PointCloud<pcl::PointXYZ>());
   data->height = 1;
   data->width  = points.size();
-  /* ROS_DEBUG("[POINTXYZ VECTOR TO POINTCLOUD] width, height: (%d, %d)", data->width, data->height); */
+  /* RCLCPP_DEBUG(this->get_logger(),"[POINTXYZ VECTOR TO POINTCLOUD] width, height: (%d, %d)", data->width, data->height); */
   data->is_dense = false;
   /* data->resize(data->width); \\ Uncommenting this leads to a bug -> pcl adds data->width number of points at the (0, 0, 0) */
   for (pcl::PointXYZ point : points) {
@@ -113,23 +113,23 @@ void PCLMap::initKDTreeSearch(pcl::PointCloud<pcl::PointXYZ>::Ptr points) {
   pcl_cloud = points; // FIXME: unnecessarry if not using check distance from nearest point
 
   if (points->size() > 0) {
-    /* ROS_INFO("[%s]: initkdtree, point size = %lu", ros::this_node::getName().c_str(), points->size()); */
+    /* RCLCPP_INFO(this->get_logger(),"initkdtree, point size = %lu",points->size()); */
     kdtree->setInputCloud(points->makeShared());
-    /* ROS_INFO("[%s]: kdtree function end", ros::this_node::getName().c_str()); */
+    /* RCLCPP_INFO(this->get_logger(),"kdtree function end"); */
     kd_tree_initialized = true;
   }
-  ROS_INFO("[%s]: init kd tree search end", ros::this_node::getName().c_str());
+  RCLCPP_INFO(this->get_logger(),"init kd tree search end");
 }
 
 void PCLMap::initKDTreeSearch() {
 
   if (pcl_cloud->size() > 0) {
-    /* ROS_INFO("[%s]: initkdtree, point size = %lu", ros::this_node::getName().c_str(), points->size()); */
+    /* RCLCPP_INFO(this->get_logger(),"initkdtree, point size = %lu", points->size()); */
     kdtree->setInputCloud(pcl_cloud);
-    /* ROS_INFO("[%s]: kdtree function end", ros::this_node::getName().c_str()); */
+    /* RCLCPP_INFO(this->get_logger(),"kdtree function end"); */
     kd_tree_initialized = true;
   }
-  ROS_INFO("[%s]: init kd tree search end", ros::this_node::getName().c_str());
+  RCLCPP_INFO(this->get_logger(),"init kd tree search end");
 
 }
 
@@ -138,7 +138,7 @@ double PCLMap::getDistanceFromNearestPoint(pcl::PointXYZ point) {
   std::vector<float> sqr_distances(1);
 
   if (kd_tree_initialized && kdtree->nearestKSearch(point, 1, indices, sqr_distances) > 0) {
-    /* ROS_INFO("[%s]: Nearest point search: returning %.2f", ros::this_node::getName().c_str(), sqrt(sqr_distances[0])); */
+    /* RCLCPP_INFO(this->get_logger(),"Nearest point search: returning %.2f", sqrt(sqr_distances[0])); */
     return sqrt(sqr_distances[0]);
   } else {
     return FLT_MAX;
@@ -159,7 +159,7 @@ bool PCLMap::checkDistanceFromNearestPoint(pcl::PointXYZ point, double safe_dist
       }
       return true;
     }
-    /* ROS_INFO("[%s]: Nearest point search: returning %.2f", ros::this_node::getName().c_str(), sqrt(sqr_distances[0])); */
+    /* RCLCPP_INFO(this->get_logger(),"Nearest point search: returning %.2f", sqrt(sqr_distances[0])); */
     return false;
   } else {
     return false;
@@ -170,12 +170,12 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr PCLMap::octomapToPointcloud(std::shared_ptr<
   std::vector<pcl::PointXYZ> output_pcl;
 
   if (map_limits[0].x() > map_limits[1].x() || map_limits[0].y() > map_limits[1].y() || map_limits[0].z() > map_limits[1].z()) { 
-    ROS_ERROR("[PCL map]: Octomap cannot be converted. Provided map limits cannot be used for definition of bounding box.");
+    RCLCPP(this->get_logger(),"[PCL map]: Octomap cannot be converted. Provided map limits cannot be used for definition of bounding box.");
     return nullptr;
   }
 
   if (!input_octree) { 
-    ROS_ERROR("[PCL map]: Octomap cannot be converted. Empty input octree received."); // FIXME add retunr
+    RCLCPP(this->get_logger(),"[PCL map]: Octomap cannot be converted. Empty input octree received."); // FIXME add retunr
   }
 
   octomap::OcTreeKey min_key = input_octree->coordToKey(map_limits[0]);
@@ -204,6 +204,6 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr PCLMap::octomapToPointcloud(std::shared_ptr<
     return pclVectorToPointcloud(output_pcl);
   }
 
-  ROS_ERROR("[PCL map]: Octomap cannot be converted empty pointcloud received.");
+  RCLCPP(this->get_logger(),"[PCL map]: Octomap cannot be converted empty pointcloud received.");
   return nullptr;
 }
